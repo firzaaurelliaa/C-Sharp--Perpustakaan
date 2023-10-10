f289294d3994847f94f7bc5684a09876fd3b50dd
firzaurellia <firzaurellia@gmail.com>
2023-10-10 21:56:20 +0700

    first commit

---
+++ entry_data_buku/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace entry_data_buku
+{
+    public partial class Form1 : Form
+    {
+        string database = "server = localhost; database=buku; uid=root; pwd='';";
+        public MySqlConnection koneksi;
+        public MySqlCommand cmd;
+        public MySqlDataAdapter adp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Query(string query)
+        {
+            koneksi = new MySqlConnection(database);
+            try
+            {
+                koneksi.Open();
+                cmd = new MySqlCommand(query, koneksi);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ali)
+            {
+                MessageBox.Show(ali.Message);
+            }
+            finally
+            {
+                koneksi.Close();
+            }
+        }
+
+
+       
+
+        public void konek()
+        {
+            koneksi = new MySqlConnection(database);
+            koneksi.Open();
+        }
+
+        public void disconek()
+        {
+            koneksi = new MySqlConnection(database);
+            koneksi.Close();
+        }
+
+        public DataTable baca()
+        {
+            string sql = "select * from databuku";
+            DataTable dt = new DataTable();
+            try
+            {
+                konek();
+                cmd = new MySqlCommand(sql, koneksi);
+                adp = new MySqlDataAdapter(cmd);
+                cmd.ExecuteNonQuery();
+                adp.Fill(dt);
+                dataGridView1.DataSource = dt;
+               
+            }
+            catch (Exception ali)
+            {
+                MessageBox.Show(ali.Message);
+            }
+            disconek();
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baca();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Query("INSERT INTO databuku VALUES('" + dateTimePicker1.Value.Date.ToString("yyyyMMdd") + "','" + this.textBox1.Text + "','" + this.textBox2.Text + "','" + this.textBox3.Text + "','" + this.textBox4.Text + "' WHERE id_buku='");
+            MessageBox.Show("Data berhasil ditambahkan", "Berhasil!",
+            MessageBoxButtons.OK);
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+           Query("UPDATE databuku set tahun_terbit = '" + dateTimePicker1.Text + "',penulis = '" + this.textBox1.Text + "',judul_buku='" + this.textBox2.Text + "',penulis='" + this.textBox3.Text + "',penerbit='" + this.textBox4.Text + "' WHERE id_buku='" );
+           MessageBox.Show("Update success");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ entry_data_buku/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("entry_data_buku")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("entry_data_buku")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("98c8bcb6-acf5-4a28-b748-cb2281a2e7ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
